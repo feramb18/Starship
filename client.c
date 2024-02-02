e949037bc500342ef7df59ce9acb9acd39a829f2
adriadamo <adriano.adamo001@studenti.uniparthenope.it>
2024-02-02 18:08:56 +0100

    aggiunta di commenti al codice

--- client.c
+++ client.c
@@ -10,29 +10,37 @@
 #define WINDOW_HEIGHT 600
 #define PORT 4550
 #define GRID_SIZE 10
-#define DEBRIS_COUNT 5
 #define MAX_DEBRIS 50 // Numero massimo di detriti che possono essere gestiti contemporaneamente
-
 
 struct Debris {
     int x, y;
     int active; // 1 se il detrito è attivo, 0 altrimenti
 };
+
 struct Spaceship {
     int x, y; // Posizione della navicella
     int width, height; // Dimensioni della navicella
 };
 
+struct Spaceship spaceship;
+
 struct Debris debris[MAX_DEBRIS]; // Array per gestire più generazioni di detriti
+
+//invio dei detriti
 struct DebrisPacket {
     struct Debris debris[MAX_DEBRIS];
 };
 
 struct DebrisPacket debrisReceivedPacket;
 
+//posizione navicella
 struct SpaceshipPosition {
     int x;
 };
+
+struct SpaceshipPosition pos;
+
+//forma dei detriti
 void drawCircle(SDL_Renderer *renderer, int centerX, int centerY, int radius) {
     for (int y = -radius; y <= radius; y++)
         for (int x = -radius; x <= radius; x++)
@@ -41,80 +49,86 @@ void drawCircle(SDL_Renderer *renderer, int centerX, int centerY, int radius) {
 }
 
 int main() {
+
     int sockfd;
     struct sockaddr_in servaddr;
-    struct Debris debris[DEBRIS_COUNT];
     SDL_Event event;
     int quit = 0;
 
-
+    //Creazione Sokcet UDP, utilizzando indirizzi AF_INTET
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("Errore nella creazione del socket");
         exit(EXIT_FAILURE);
     }
 
+    //Configurazione indirizzo socket del server
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr("192.168.1.146"); //ip
+    servaddr.sin_addr.s_addr = inet_addr("192.168.1.149"); //ip
     servaddr.sin_port = htons(PORT);
 
-    //implementazione libreria SDL//
+    //implementazione libreria SDL
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("Starship", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH,
-                                          WINDOW_HEIGHT, 0);
+    SDL_Window *window = SDL_CreateWindow("Starship", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH,WINDOW_HEIGHT, 0);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    struct Spaceship spaceship;
+    //Creazione navicella
     spaceship.x = WINDOW_WIDTH / 2; // Posiziona la navicella al centro in orizzontale
     spaceship.y = WINDOW_HEIGHT - 50; // Posiziona la navicella verso il fondo della finestra
-    spaceship.width = 50; // Imposta una larghezza
-    spaceship.height = 30; // Imposta un'altezza
+    spaceship.width = 50; // Larghezza navicella
+    spaceship.height = 30; //Altezza navicella
 
     int verticalMargin = 500; // Margine verticale sopra l'astronave per attivare l'allarme
     while (!quit) {
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Imposta il colore di sfondo su nero
-        SDL_RenderClear(renderer); // Pulisci il frame corrente
-        struct SpaceshipPosition pos;
-        // Gestione degli eventi di input
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); //Sfondo nero
+        SDL_RenderClear(renderer); // Pulizia frame corrente
+
+        // Gestione eventi di input
         while (SDL_PollEvent(&event) != 0) {
             if (event.type == SDL_QUIT) {
                 quit = 1;
             } else if (event.type == SDL_KEYDOWN) {
                 switch (event.key.keysym.sym) {
+
+                    //Input tastiera
                     case SDLK_LEFT:
-                        spaceship.x -= 100; // Sposta a sinistra
-                        // Assicurati che la navicella non esca dallo schermo sulla sinistra
+                        spaceship.x -= 100; //Sposta la navicella a sinistra
+                        //Controllo per non far uscire la navicella dallo schermo
                         if (spaceship.x < 0) spaceship.x = 0;
                         break;
 
+                    //Input tastiera
                     case SDLK_RIGHT:
-                        spaceship.x += 100; // Sposta a destra
-                        // Assicurati che la navicella non esca dallo schermo sulla destra
+                        spaceship.x += 100; //Sposta la navicella a destra
+                        //Controllo per non far uscire la navicella dallo schermo
                         if (spaceship.x > WINDOW_WIDTH - spaceship.width) {
                             spaceship.x = WINDOW_WIDTH - spaceship.width;
                         }
                         break;
-
+                    //Input Tastiera
                     case SDLK_ESCAPE:
                         quit = 1; // Premendo "Esc", esci dal gioco
                         break;
                 }
             }
         }
-// Aggiorna la posizione della navicella da inviare al server
-                pos.x = spaceship.x;
-// Invia la posizione aggiornata al server
-printf("invio in posizione %d",pos.x);
-sendto(sockfd, &pos, sizeof(pos), 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
-   // Ricezione dei detriti dal server
+        //Aggiorna la posizione della navicella da inviare al server
+        pos.x = spaceship.x;
+        printf("invio in posizione %d\n",pos.x);
+
+        //Invia la posizione aggiornata al server
+        sendto(sockfd, &pos, sizeof(pos), 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+        // Ricezione dei detriti dal server
    int receivedDebrisCount=recvfrom(sockfd, &debrisReceivedPacket, sizeof(struct DebrisPacket), 0, NULL,NULL);
                 if (receivedDebrisCount < 0) {
             perror("recvfrom failed");
             continue;
         }
-        int alert=0; // Resetta l'allarme per ogni frame
-        // Rendering dei detriti
+                int alert=0; // Flag che parte da 0, se il detrito è in traiettoria diventa 1
+        //Rendering dei detriti
         for (int i = 0; i < receivedDebrisCount; i++) {
             if (debrisReceivedPacket.debris[i].active) {
                 int centerX = debrisReceivedPacket.debris[i].x * (WINDOW_WIDTH / GRID_SIZE) + (WINDOW_WIDTH / GRID_SIZE / 2);
@@ -123,33 +137,21 @@ sendto(sockfd, &pos, sizeof(pos), 0, (struct sockaddr *)&servaddr, sizeof(servad
                 // Verifica se il detrito è entro il margine verticale sopra l'astronave
                 if (centerY >= spaceship.y - verticalMargin && centerY < spaceship.y + spaceship.height) {
                     if (centerX >= spaceship.x && centerX <= spaceship.x + spaceship.width) {
-                        alert = 1; // Attiva l'allarme se il detrito è direttamente sopra l'astronave entro il margine verticale
+                        alert = 1; // Attiva l'allarme se il detrito è sopra l'astronave
                     }
                 }
 
-                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Imposta il colore dei detriti su bianco
-                drawCircle(renderer, centerX, centerY, 20); // Disegna il detrito come un cerchio
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); //colore dei detriti bianco
+                drawCircle(renderer, centerX, centerY, 20); //Chiamata per disegnare il detrito
             }
         }
 
         // Rendering dell'astronave
         SDL_Rect spaceshipRect = {spaceship.x, spaceship.y, spaceship.width, spaceship.height};
-        int score=0;
-        for (int i = 0; i < DEBRIS_COUNT; i++) {
-            SDL_Rect debrisRect = {debris[i].x * (WINDOW_WIDTH / GRID_SIZE), debris[i].y * (WINDOW_HEIGHT / GRID_SIZE), 30, 30}; // Approssima i detriti come rettangoli per la collisione
-            if (SDL_HasIntersection(&spaceshipRect, &debrisRect)) {
-                printf("Collision detected!\n"); // Gestisci la collisione qui
-                score=score+1; // Decommenta per terminare il gioco in caso di collisione
-            }else{
-                score=score-10;
-            }
-            //printf("\nScore:\n %d",score);
-        }
         if (alert) {
-            SDL_SetRenderDrawColor(renderer, 255, 0, 0,
-                                   255); // Imposta il colore dell'astronave su rosso in caso di allarme
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0,255); // Imposta il colore dell'astronave su rosso in caso di allarme
         } else {
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Altrimenti, usa il colore bianco
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); //Usa il colore bianco in assenza di detriti
         }
         SDL_RenderFillRect(renderer, &spaceshipRect); // Disegna l'astronave
 
@@ -160,7 +162,7 @@ sendto(sockfd, &pos, sizeof(pos), 0, (struct sockaddr *)&servaddr, sizeof(servad
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-    close(sockfd);
+    close(sockfd); //Chiusura socket
 
     return 0;
 }
--- server.c
+++ server.c
@@ -11,21 +11,45 @@
 #define PORT 4550
 #define GRID_SIZE 10
 #define MAX_DEBRIS 20
-#define MAX_DETRITI 3
+#define MAX_DETRITI 2 //Massimo dei detriti generati nelle funzioni
 #define WINDOW_HEIGHT 10
 #define DEBRIS_GENERATION_INTERVAL 2000000 // 2 secondi in microsecondi
+
+//Struct
+//informazioni sull'indirizzo e la porta di un socket
+struct sockaddr_in servaddr;
+
+//Gestione del tempo
+struct timeval lastDebrisTime, currentTime;
+
+//rappresentazione tempo in secondi
+struct timeval tv;
+
+//informazioni sull'indirizzo e la porta di un client o di un socket remoto
+struct sockaddr_in clientAddr;
+
+//Posizione navicella
+struct SpaceshipPosition pos;
+
 struct Debris {
     int x, y;
     int active;
 };
+
 struct DebrisPacket {
     struct Debris debris[MAX_DEBRIS];
 };
+
 struct Debris debris[MAX_DEBRIS];
+
 struct SpaceshipPosition {
     int x;
 };
 
+struct DebrisPacket packet;
+
+//Function
+//Inizializza i detriti
 void initDebris() {
     int count = 0;
     for (int i = 0; i < GRID_SIZE; i++) {
@@ -37,6 +61,8 @@ void initDebris() {
         }
     }
 }
+
+//Generazione detriti in traiettoria della navicella
 void generateDebrisBasedOnSpaceship(int pos)
  {
     int count = 0;
@@ -49,21 +75,24 @@ void generateDebrisBasedOnSpaceship(int pos)
         }
     }
 }
-
+//Aggiornamento posizioni detriti attivi, spostandoli una volta arrivati verso il basso, in alto, in posizione randomica
 void updateDebrisPositions() {
+    int count = 0;
     for (int i = 0; i < GRID_SIZE; i++) {
         if (debris[i].active) {
             debris[i].y += 1;
-            if (debris[i].y > WINDOW_HEIGHT) {
+            if (debris[i].y > WINDOW_HEIGHT && count < MAX_DEBRIS) {
                 debris[i].y = 0; // Riporta il detrito in cima per simulare un flusso continuo
-                debris[i].x = rand() % GRID_SIZE; // Cambia la posizione x per varietà
+                debris[i].x = rand() % GRID_SIZE; // Cambia la posizione x in modo randomico
+                count++;
             }
         }
     }
 }
 
+//Invio "detriti" attivi nell'array debris al client, attraverso una socket UDP, inserendoli in una struct DebrisPacket
 void sendDebrisPacket(int sockfd, struct sockaddr_in *clientAddr) {
-    struct DebrisPacket packet;
+
     for (int i = 0; i < GRID_SIZE; i++) {
        if (debris[i].active) {
            packet.debris[i] = debris[i];
@@ -77,55 +106,64 @@ void sendDebrisPacket(int sockfd, struct sockaddr_in *clientAddr) {
 
 int main() {
     int sockfd;
-    struct sockaddr_in servaddr;
-    struct timeval lastDebrisTime, currentTime;
-    fd_set writefds;
-    struct timeval tv;
+    fd_set writefds; //Per tenere traccia dei descrittori di file, dove monitoriamo eventi di scrittura, in questo caso lo spawn dei detriti in direzione della navicella
     int retval;
     tv.tv_sec = 5;
     tv.tv_usec = 0;
 
+    //Creazione socket UDP e stampa in cso di errori
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-
     if (sockfd < 0) {
         perror("Errore nella creazione del socket");
         exit(EXIT_FAILURE);
     }
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(PORT);
 
+    //Configurazione socket impostando indirizzo e porta
+    memset(&servaddr, 0, sizeof(servaddr)); //inizializzazione
+    servaddr.sin_family = AF_INET; //imposto famiglia indirizzi a AF_INET
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); //Server in ascolto su tutte le interfaccie di rete
+    servaddr.sin_port = htons(PORT); //Impostazione porta del socket
+
+    //Associazione (binding) del socket ad un indirizzo e porta speicifica
     if (bind(sockfd, (const struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
         perror("Binding fallito");
         exit(EXIT_FAILURE);
     }
 
+    //Inizilizzazione di generazione di numeri casuali
     srand(time(NULL));
-    memset(debris, 0, sizeof(debris));
-    gettimeofday(&lastDebrisTime, NULL);
-    struct sockaddr_in clientAddr;
-    socklen_t clientAddrLen = sizeof(clientAddr);
-    ssize_t bytesReceived;
-    struct SpaceshipPosition pos;
+
+
+    memset(debris, 0, sizeof(debris)); //iniziallizazione array debris con zeri
+    gettimeofday(&lastDebrisTime, NULL); //Otteniamo l'ora corrente
+    socklen_t clientAddrLen = sizeof(clientAddr); //Utilizziamo la dimensione di clientaddr come argomento, in modo che la func sappiamo quanto spazio di memoria è stato allocato per l'indirizzo del client
+
     initDebris();
+
     // Azzera l'insieme di file descriptor e aggiunge il socket
     FD_ZERO(&writefds);
     FD_SET(sockfd, &writefds);
 
     while (1) {
-        retval = select(sockfd + 1, NULL, &writefds, NULL, &tv);
+        retval = select(sockfd + 1, NULL, &writefds, NULL, &tv); //verifica se il socket è pronto all'invio dei dati
+        //verifica se c'è errore con la restituzione di -1
         if (retval == -1) {
             perror("select()");
-        } else if (retval) {
-        recvfrom(sockfd, &pos, sizeof(pos), 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
+
+        }
+
+        else if (retval) {
+            //Pronto per l'invio dei dati
+        recvfrom(sockfd, &pos, sizeof(pos), 0, (struct sockaddr*)&clientAddr, &clientAddrLen); //ricezione dati dal socket
             generateDebrisBasedOnSpaceship(pos.x);
-            printf("ricevo in posizione %d",pos.x);
+            printf("ricevo in posizione %d\n",pos.x);
 
         } else {
-            // Nessun dato disponibile: genera detriti casuali
+            //Nessun dato disponibile, quindi genera detriti casuali
             initDebris();
         }
+
+        //Caclola il tempo tracorso tra gli eventi, impostato a 2 secondi
         gettimeofday(&currentTime, NULL);
         long elapsedTime = (currentTime.tv_sec - lastDebrisTime.tv_sec) * 1000000L + (currentTime.tv_usec - lastDebrisTime.tv_usec);
 
@@ -134,8 +172,10 @@ int main() {
         }
 
         updateDebrisPositions();
+        //Invio pacchetti al client
         sendDebrisPacket(sockfd, &clientAddr);
-        usleep(50000); // Controlla la velocità di aggiornamento dei detriti
+        // Controlla la velocità di aggiornamento dei detriti
+        usleep(50000);
     }
 
     return 0;
